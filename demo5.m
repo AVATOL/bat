19626772fedba085c1522d5a16f173318e855b30
hushell <dom343@gmail.com>
2014-04-15 14:38:37 -0700

    rpm init

--- demo5.m
+++ demo5.m
@@ -38,7 +38,7 @@ pos = pointtobox(pos,Species.parent,Species.bb_const1,Species.bb_const2);
 neg = getNegativeData([Species.rt_dir,'neg/'],'png');
 
 % visualize training data
-show_data = 1;
+show_data = 0;
 if (show_data == 1)
     % show data
     for i=1:length(pos)
@@ -74,4 +74,87 @@ for ti = 1:length(testX)
     pause;
 end
 
+%% getting initialization
+% im = imread(testX(1).im);
+% [~,~,boxes,msgs,parts,bkptr] = detect_fast_initmodel(im, model);
+numparts = Species.num_parts;
+allov = ones(length(pos),numparts)*1e10;
+allovu = ones(length(pos),numparts)*1e10;
+for ti = 1:length(pos)
+    im = imread(pos(ti).im);
+    [allov(ti,:),allovu(ti,:)] = detect_fast_initmodel(im, model);
+end
+ov = mean(allov,1);
+ovu = mean(allovu,1);
 
+model.ominode = struct('w',{},'i',{});
+model.omiedge = struct('w',{},'i',{});
+for k = 1:numparts
+  nb = length(model.ominode);
+  b.w = ov(k);
+  b.i = model.len + 1;
+  model.ominode(nb+1) = b;
+  model.len = model.len + numel(b.w);
+  model.components{1}(k).onid = nb+1;
+  
+  if k > 1
+    nb = length(model.omiedge);
+    bb.w = ovu(k);
+    bb.i = model.len + 1;
+    model.omiedge(nb+1) = bb;
+    model.len = model.len + numel(bb.w);
+    model.components{1}(k).omid = nb+1;
+  end
+end
+
+model.adj = zeros(length(model.pa));
+for k = 2:length(model.pa)
+  model.adj(k,model.pa(k)) = 1;
+end
+
+% im = imread(testX(1).im);
+% pyra = featpyramid(im,model);
+% [components,filters,resp] = parsemodel(model,pyra);
+
+patterns = cell(length(pos),1);
+labels = cell(length(pos),1);
+
+for i = 1:1  
+  im = imread(pos(i).im);
+  bbox = [pos(i).x1' pos(i).y1' pos(i).x2' pos(i).y2'];
+  B = [pos(i).x1;pos(i).y1;pos(i).x2;pos(i).y2];
+%   B = reshape(B,[4*length(Species.parent),1])';
+  C = B(:,6);
+  im(C(2):C(4),C(1):C(3),:) = 0;
+  im(C(2)+20:C(4)+20,C(1):C(3),:) = 0;
+  
+  pyra = featpyramid(im, model); 
+  patterns{i}.pyra = pyra;
+  %bbox = [reshape(bbox',1,4*length(pos(i).x1)) 1 0];
+  labels{i}.bbox = bbox;
+end
+
+param.overlap   = 0.5;
+param.overlap1   = param.overlap / 2;
+param.fix_def   = 0;
+label = rpm_oracle2(param, model, patterns{1}, labels{1});
+showboxes(im,label.bbox,Species.part_color);
+
+i = 2;
+im = imread(pos(i).im);
+bbox = [pos(i).x1' pos(i).y1' pos(i).x2' pos(i).y2'];
+B = [pos(i).x1;pos(i).y1;pos(i).x2;pos(i).y2];
+%   B = reshape(B,[4*length(Species.parent),1])';
+
+for j = 1:numparts
+  disp(['part ' num2str(j)]);
+  bim = im;
+  C = ceil(B(:,j));
+  bim(max(1,C(2)-10):C(4)+10,max(1,C(1)-10):C(3)+10,:) = 0;
+  pyra = featpyramid(bim, model); 
+  pat.pyra = pyra;
+  lab.bbox = bbox;
+  label = rpm_oracle2(param, model, pat, lab);
+  showboxes(bim,label.bbox,Species.part_color);
+  pause
+end
---
+++ rpm/rpm_oracle2.m
@@ -0,0 +1,440 @@
+function label = rpm_oracle2(param, model, xi, yi)
+% do loss-augmented detection on a given example (xi,yi) using
+% model.w as parameter. Param is ignored (included for standard
+% interface). The loss used is normalized Hamming loss.
+% 
+% If yi is not given, then standard prediction is done (i.e. MAP decoding
+% without the loss term).
+%
+% INPUT: 
+% param = {overlap, thresh, latent}, where latent == loss-aug
+% model
+% xi = {pyra}
+% yi = {bbox}
+% OUTPUT:
+% label = {bbox, level, ex}
+%
+% TODO: support mixture parts
+%%
+global Ix_cache Iy_cache It_cache Im_cache Is_cache comb_cache
+
+INF = 1e10;
+latent = false;
+overlap = param.overlap;
+overlap1 = param.overlap1;
+%thresh = param.thresh;
+
+pyra = xi.pyra;
+
+if nargin > 3 && ~isempty(yi.bbox)
+  % latent means to do loss augmentation
+  latent = true;
+  %thresh = -INF;
+  bbox = yi.bbox;
+end
+
+interval = model.interval;
+levels = 1:length(pyra.feat);
+
+% get model components
+%*** model = vec2model(model.w, model); % NOTE: update model.filter to do inference
+[components,filters,resp] = parsemodel(model,pyra);
+boxes = zeros(length(levels),length(components{1})*4+2);
+exs = cell(1,length(levels));
+ptr = cell(1,length(levels));
+subadj = cell(1,length(levels));
+ex.blocks = [];
+
+% DEBUG
+allskipflags = zeros(1,length(levels));
+
+% Iterate over random permutation of scales and components,
+for rlevel = levels
+  % Iterate through mixture components (viewpoints)
+  for c  = randperm(length(model.components))
+    parts = components{c};
+    numparts = length(parts);
+
+    % Skip if there is no overlap of root filter with bbox
+    % TODO: overlap is critical, handling large-scale windows problematic
+    if latent
+      skipflag = 0;
+      for k = 1:numparts
+        % because all mixtures for one part is the same size, we only need to do this once
+        ovmask = testoverlap(parts(k).sizx(1),parts(k).sizy(1),...
+          pyra,rlevel,bbox(k,:),overlap);
+        if ~any(ovmask)
+          skipflag = 1;
+          break;
+        end
+      end
+      if skipflag == 1
+        allskipflags(rlevel) = 1;
+        continue;
+      end
+    end % latent
+    
+    % local scores and loss augmentation
+    for k = 1:numparts
+      f = parts(k).filterid;
+      level = rlevel-parts(k).scale*interval; % scale is set to 0, so parts in same level as root
+      if isempty(resp{level})
+        resp{level} = fconv(pyra.feat{level},filters,1,length(filters));
+      end
+
+      for fi = 1:length(f) % mixtures
+        % loss augment
+        if latent
+            ovmask = testoverlap(parts(k).sizx(fi),parts(k).sizy(fi),pyra,rlevel,bbox(k,:),overlap);
+            %ovmask1 = testoverlap(parts(k).sizx(fi),parts(k).sizy(fi),pyra,rlevel,bbox(k,:),overlap1);
+            %ovmask = ~ovmask == ovmask1;
+            ovmask = ~ovmask;
+            resp{level}{f(fi)}(ovmask) = resp{level}{f(fi)}(ovmask) + 1/numparts;
+        end       
+        % theta_v(yv,xv,tv,kv)
+        parts(k).score(:,:,2,fi) = resp{level}{f(fi)}; % tv = 2
+        parts(k).score(:,:,1,fi) = parts(k).on(fi); % tv = 1
+      end % fi
+      parts(k).level = level;
+      
+      if all(cellfun(@isempty, resp))
+        error('!!!!! No quantized location overlap with bbox !!!!!!');
+      end
+      
+      % TODO: check if needed to do sth for mix parts
+      
+    end % for numparts local scores 
+    
+    % Walk from leaves to root of tree, passing message to parent
+    adj = model.adj;
+    addpath util/graph/
+    tporder = topological_sort(adj);
+    msg_cache = cell(size(adj));
+    Ix_cache = cell(size(adj));
+    Iy_cache = cell(size(adj));
+    It_cache = cell(size(adj));
+    Im_cache = cell(size(adj));
+    Is_cache = cell(1,size(adj,1));
+    comb_cache = cell(1,size(adj,1));
+    
+    for k = tporder % make sure this is reverse of topological order
+      % compute score of itself by adding coming messages (decide t_vu)
+      % score_v(yv,xv,tv,kv) 
+      % = theta_v(yv,xv,tv,kv) + max_{tv.} sum_u msg_{u->v}(yv,xv,tv,tvu,kv)
+      % theta_v(yv,xv,tv,kv) = w_v^{kv}*phi(yv,xv)   if tv = 2
+      %                      = o_v^{kv}              if tv = 1
+      % backpointer: Is(yv,xv,tv,kv) = {tv.}^*(yv,xv,tv,kv)
+      Ny  = size(parts(k).score,1);
+      Nx  = size(parts(k).score,2); 
+      L = length(parts(k).filterid);
+      
+      children = adj(:,k) == 1;
+      combs = cartesianProduct(repmat({[0+1 1+1]},[1 sum(children)])); % tvu
+      cmsgs = msg_cache(children,k);
+      
+      ncb = max(1,size(combs,1));
+      %Is = zeros(Ny,Nx,2,L);
+      comb_msg = zeros(Ny,Nx,2,L,ncb);
+      % TODO: now DAG is assumed to be a tree. check tree constraint (single parent)
+      for ci = 1:size(combs,1)
+        for u = 1:size(combs,2)
+          if isempty(cmsgs{u})
+            continue
+          end
+          comb_msg(:,:,:,:,ci) = comb_msg(:,:,:,:,ci) + squeeze(cmsgs{u}(:,:,:,combs(ci,u),:));
+        end
+      end
+      [comb_msg,Is_cache{k}] = max(comb_msg,[],5);
+      if ~isempty(comb_msg)
+        parts(k).score = parts(k).score + comb_msg;
+      end
+      comb_cache{k} = combs;
+      
+      % compute forward messages to all parents
+      % msg_{u->v}(yv,xv,tv,tvu,kv)
+      if (param.fix_def)
+        parts(k).w = repmat([0.01 0 0.01 0]', [1,size(parts(k).defid)]);
+      end
+      par = parts(k).parent;
+      assert(~isempty(par));
+      
+      % root: no passing only add bias
+      % score_r(yr,xr,tr,kr) += b_r(:,1,1)
+      % TODO: every node adds a bias
+      if par == 0
+        %b = parts(k).b(:,1,1);
+        %b = shiftdim(b,-3);
+        %parts(k).score = bsxfun(@plus,parts(k).score,b);
+        continue
+      end
+      
+      % non-root: pass messages to all parents
+      assert(all(par > 0));
+      for pp = par
+        [msg_cache{k,pp},Ix_cache{k,pp},Iy_cache{k,pp},It_cache{k,pp},Im_cache{k,pp}] ...
+          = passmsg(parts(k),parts(pp));
+      end
+    end % message passing
+
+    % find max location for each root and use the root with max score
+    val = -INF; [Y,X,It,Im] = deal([]);
+    for k = 1:numparts
+      if any(parts(k).parent > 0)
+        continue
+      end
+      [rscore1 tIm] = max(parts(k).score,[],4);
+      [rscore tIt] = max(rscore1,[],3);
+      [tval,tind] = max(rscore(:));
+      if tval > val
+        val = tval;
+        [Y,X] = ind2sub(size(rscore),tind);
+        Im = tIm; % Im = (Ny,Nx,Dv)
+        It = tIt; % It = (Ny,Nx)
+        Is = Is_cache{k};
+      end
+    end
+    
+    % Walk back down tree following pointers
+    for i = 1:length(X) % TODO: length correct?
+      x = X(i);
+      y = Y(i);
+      tr = It(y,x); % t_r
+      m = Im(y,x,tr); % mixture
+      ts = Is(y,x,tr,m);
+      [box,exs{rlevel},ptr{rlevel},subadj{rlevel}] = backtrack(x,y,tr,m,ts,adj,parts,pyra,ex,0); % write = latent
+      boxes(rlevel,:) = [box c rscore(y,x)];
+    end
+  end % c
+end % rlevel
+
+if all(boxes == 0)
+  error('!!! All boxes are empty !!!\n');
+end
+
+%boxes = boxes(1:cnt,:);
+if latent && ~isempty(boxes)
+  [~,ii] = max(boxes(:,end)); % TODO: test which better: max or random (ii = end)
+  boxes = boxes(ii,:);
+  label.level = ii;
+  label.ex = exs{label.level};
+  label.ptr = ptr{label.level};
+  label.subadj = subadj{label.level};
+  % DEBUG
+  if isempty(label.ex)
+    dbstop
+  end
+end
+
+%[boxes] = nms(boxes,0.3);
+
+assert(~isempty(boxes));
+label.bbox = boxes(1,:); % TODO: boxes can be empty
+
+
+%% helper functions
+
+function [msg,Ix,Iy,It,Ik] = passmsg(child,parent)
+% Given a 2D array of filter scores 'child',
+% (1) Apply distance transform
+% (2) Shift by anchor position of part wrt parent
+% (3) Downsample if necessary
+% msg_{u->v}(yv,xv,tv,tvu,kv) 
+% = max_ku [ b_vu(kv,ku)
+%            + max_{yu,xu,tu} ( theta(yv,xv,yu,xu,tv,tu,tvu,kv,ku)
+%                               + score_u(yu,xu,tu,ku) ) ]
+% backpointers: (1) Ix, Iy <- xu^*, yu^*
+%               (2) It <- tu^*
+%               (3) Ik <- ku^*
+% Note that all backpointers have the same dim as msg_{u->v}
+% e.g., It(yv,xv,tv,tvu,kv) = tu^*(yv,xv,tv,tvu,kv)
+
+whichpar = parent.id == child.parent;
+assert(sum(whichpar) == 1);
+K   = length(child.filterid);
+Ny  = size(parent.score,1);
+Nx  = size(parent.score,2);  
+Du  = 2;
+Duv = 2;
+[Ix0,Iy0,It0,msg0] = deal(zeros([Ny Nx Du Duv K]));
+
+% max_tu,xu, iff tv = tu = tvu = 2, theta_vu will be used, otherwise o_vu 
+for tv = 1:Du
+  for tvu = 1:Duv
+    for k = 1:K
+      % if o_vu used, max is indep of yv,xv,tv,tvu,kv
+      oscore = zeros(1,Du);
+      oind = zeros(1,Du);
+      
+      for tu = 1:Du % exception tv=tvu=2 later
+        childsc = child.score(:,:,tu,k); % score_u(yu,xu,tu,ku)
+        [oscore(tu),oind(tu)] = max(childsc(:));
+      end % for tu
+
+      % oscore = (Ny,Nx,Du)
+      oscore = shiftdim(oscore,-1);
+      oind = shiftdim(oind,-1);
+      oscore = oscore + child.om(k,whichpar); % o_vu(k,v)
+      oscore = repmat(oscore,[Ny Nx]);
+      oind = repmat(oind,[Ny Nx]);
+      [oiy,oix] = ind2sub([Ny Nx],oind);
+
+      if tv == 2 && tvu == 2
+        if child.w(1,k,whichpar) == 0
+          child.w(1,k,whichpar) = child.w(1,k,whichpar) + 1e-5;
+        end
+        if child.w(3,k,whichpar) == 0
+          child.w(3,k,whichpar) = child.w(3,k,whichpar) + 1e-5;
+        end
+        w1 = child.w(1,k,whichpar); w2 = child.w(2,k,whichpar); 
+        w3 = child.w(3,k,whichpar); w4 = child.w(4,k,whichpar);
+        stx = child.startx(k,whichpar); sty = child.starty(k,whichpar);
+        % tu = 2
+        [dtscore,dtix,dtiy] = shiftdt(child.score(:,:,2,k),w1,w2,w3,w4,stx,sty,Nx,Ny,child.step);
+        oscore(:,:,2) = dtscore;
+        oiy(:,:,2) = dtiy;
+        oix(:,:,2) = dtix;
+      end % if
+
+      [msg0(:,:,tv,tvu,k),itu] = max(oscore,[],3); % return Ny by Nx matrix
+      It0(:,:,tv,tvu,k) = itu;
+      N = Ny*Nx;
+      i0 = reshape(1:N,Ny,Nx);
+      ind = i0 + N*(itu-1); % think about if itu are all 1 or 2
+      Ix0(:,:,tv,tvu,k) = oix(ind); % since ind is Ny by Nx, so as oix(ind)
+      Iy0(:,:,tv,tvu,k) = oiy(ind);
+    end % for k
+  end % for tvu
+end % for tv
+
+% b_{ij}^{ki,kj}
+% At each parent location, for each parent mixture 1:L, compute best child mixture 1:K
+L  = length(parent.filterid);
+N  = Nx*Ny*Du*Duv;
+i0 = reshape(1:N,[Ny Nx Du Duv]);
+[msg,Ix,Iy,It,Ik] = deal(zeros([Ny Nx Du Duv L]));
+
+for l = 1:L
+	b = child.b(:,l,whichpar);
+	b = shiftdim(b,-4); % [b1; b2] to [b1 b2] is shiftdim(b,-1)
+	[msg(:,:,:,:,l),I] = max(bsxfun(@plus,msg0,b),[],5);
+	i = i0 + N*(I-1); % i is 4-dim array
+	Ix(:,:,:,:,l)    = Ix0(i);
+	Iy(:,:,:,:,l)    = Iy0(i);
+  It(:,:,:,:,l)    = It0(i);
+	Ik(:,:,:,:,l)    = I;
+end
+
+
+% Backtrack through dynamic programming messages to estimate part locations
+% and the associated feature vector  
+function [box,ex,ptr,subadj] = backtrack(x,y,tv,mix,ts,adj,parts,pyra,ex,write)
+% (x,y) is root location
+% ex: feature map
+
+global Ix_cache Iy_cache It_cache Im_cache Is_cache comb_cache
+
+numparts = length(parts);
+ptr = zeros(numparts,4);
+box = zeros(numparts,4);
+%combs = cell(numparts,1);
+flags = zeros(numparts,1);
+% fifo = []; % BFS
+subadj = zeros(size(adj));
+
+torder = topological_sort(adj);
+k   = torder(end);
+flags(k) = 2;
+children = find(adj(:,k) == 1);
+ptr(k,:) = [x y tv mix];
+subadj(children,k) = comb_cache{k}(ts,:)';
+
+% fifo = [fifo children(combs)];
+comb = comb_cache{k}(ts,:) == 2;
+flags(children(comb)) = 2;
+flags(children(~comb)) = 1;
+%subadj(children(comb),k) = 1;
+p   = parts(k);
+
+scale = pyra.scale(p.level);
+x1  = (x - 1 - pyra.padx)*scale+1;
+y1  = (y - 1 - pyra.pady)*scale+1;
+x2  = x1 + p.sizx(mix)*scale - 1;
+y2  = y1 + p.sizy(mix)*scale - 1;
+box(k,:) = [x1 y1 x2 y2];
+
+if write % TODO: p.xxI and check where ex has been used
+	%ex.id(3:5) = [p.level round(x+p.sizx(mix)/2) round(y+p.sizy(mix)/2)];
+  ex.level = p.level;
+	ex.blocks = [];
+	ex.blocks(end+1).i = p.biasI;
+	ex.blocks(end).x   = 1;
+	f  = pyra.feat{p.level}(y:y+p.sizy(mix)-1,x:x+p.sizx(mix)-1,:);
+	ex.blocks(end+1).i = p.filterI(mix);
+	ex.blocks(end).x   = f;
+end
+
+for k = torder(end-1:-1:1)
+  assert(flags(k) > 0);
+  
+  children = find(adj(:,k) == 1);
+  p   = parts(k);
+  par = p.parent;
+	x   = ptr(par,1);
+	y   = ptr(par,2);
+  tv  = ptr(par,3);
+	mix = ptr(par,4);
+  tvu = subadj(k,par);
+  
+  ptr(k,1) = Ix_cache{k,par}(y,x,tv,tvu,mix);
+	ptr(k,2) = Iy_cache{k,par}(y,x,tv,tvu,mix);
+	ptr(k,3) = It_cache{k,par}(y,x,tv,tvu,mix);
+  ptr(k,4) = Im_cache{k,par}(y,x,tv,tvu,mix);
+  
+	scale = pyra.scale(p.level);
+	x1  = (ptr(k,1) - 1 - pyra.padx)*scale+1;
+	y1  = (ptr(k,2) - 1 - pyra.pady)*scale+1;
+	x2  = x1 + p.sizx(ptr(k,4))*scale - 1;
+	y2  = y1 + p.sizy(ptr(k,4))*scale - 1;
+	box(k,:) = [x1 y1 x2 y2];
+  
+  if isempty(children) 
+    continue
+  end
+  ts = Is_cache{k}(y,x,tv,mix);
+  subadj(children,k) = comb_cache{k}(ts,:)';
+
+  % fifo = [fifo children(combs)];
+  comb = comb_cache{k}(ts,:) == 2;
+  flags(children(comb)) = 2;
+  flags(children(~comb)) = 1;
+	
+	if write
+		ex.blocks(end+1).i = p.biasI(mix,ptr(k,3));
+		ex.blocks(end).x   = 1;
+		ex.blocks(end+1).i = p.defI(ptr(k,3));
+		ex.blocks(end).x   = defvector(x,y,ptr(k,1),ptr(k,2),ptr(k,3),p);
+		x   = ptr(k,1);
+		y   = ptr(k,2);
+		mix = ptr(k,3);
+		f   = pyra.feat{p.level}(y:y+p.sizy(mix)-1,x:x+p.sizx(mix)-1,:);
+		ex.blocks(end+1).i = p.filterI(mix);
+		ex.blocks(end).x = f;
+	end
+end
+box = reshape(box',1,4*numparts);
+
+% enumerate all combinations equivalent tp cartesian product
+function combs = cartesianProduct(domains)
+% e.g., domains = repmat({[0 1]}, [1,3])
+if isempty(domains)
+  combs = [];
+  return
+end
+if size(domains,2) == 1
+  combs = domains{1}';
+  return
+end
+combs = cell(size(domains));
+[combs{:}] = ndgrid(domains{:});
+combs = cell2mat(cellfun(@(x) x(:), combs, 'UniformOutput',false));
+combs = sortrows(combs, 1:numel(domains));
