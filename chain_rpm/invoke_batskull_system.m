21be7919f8b4a46e7c9dd0ef71f5c9d42c01c295
hushell <dom343@gmail.com>
2015-02-09 11:17:20 -0800

    path issues

--- chain_rpm/invoke_batskull_system.m
+++ chain_rpm/invoke_batskull_system.m
@@ -7,4 +7,14 @@ if nargin == 0
     det_results = '/home/hushell/working/git-dir/avatol_cv/matrix_downloads/BAT/detection_results/DPM/c427749c427751c427753c427754c427760/v3540/split_0.7/';
 end
 
+% Change the current folder to the folder of this m-file.
+if(~isdeployed)
+    want_to_go = fileparts(which('avatol_main.m'));
+    where_we_are = cd(want_to_go);
+    fprintf('*** now we cd to %s\n', want_to_go); 
+end
+
 avatol_main(input_dir, output_dir, det_results); % regime 2
+
+cd(where_we_are);
+fprintf('*** now we back to %s\n', where_we_are); 
