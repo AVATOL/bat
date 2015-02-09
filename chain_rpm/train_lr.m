5ebf5b320a63fba4b8c5b97475e7e54af46d2aab
hushell <dom343@gmail.com>
2015-02-09 11:26:05 -0800

    train_lr get rid of foptions

--- chain_rpm/train_lr.m
+++ chain_rpm/train_lr.m
@@ -10,7 +10,10 @@ t = id(y,:);
 
 net = glm(D, nclass, 'logistic');
 
-options = foptions;
+OPTIONS=zeros(1,18);
+default_options=[0,1e-4,1e-4,1e-6,0,0,0,0,0,0,0,0,0,0,0,1e-8,0.1,0];
+OPTIONS=OPTIONS+(OPTIONS==0).*default_options;
+options = OPTIONS;
 options(1) = 1; % set to 1 to display error values during training
 options(14) = 10; %  maximum number of iterations 
 
